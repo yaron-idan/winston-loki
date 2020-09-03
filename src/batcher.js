68d8601d242e2a88a2575ec8dda4c10487f5820d
Yaron Idan <yaronidan@gmail.com>
2020-09-03 11:19:15 +0300

    Update batcher.js

--- src/batcher.js
+++ src/batcher.js
@@ -195,10 +195,11 @@ class Batcher {
             reject(err)
           }
         }
-
+        console.log("about to post to loki")
         // Send the data to Grafana Loki
         req.post(this.url, this.contentType, this.options.headers, reqBody)
           .then(res => {
+            console.log(JSON.stringify(res,null,2))
             // No need to clear the batch if batching is disabled
             logEntry === undefined && this.clearBatch()
             resolve()
